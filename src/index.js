e13e58b07644bb8b20bacd7d08a413224b192a04
Andrew4366A <andrewabi2003@gmail.com>
2026-02-15 19:31:42 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening to http://localhost:${PORT}`);
 });
+
